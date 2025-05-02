ab1e3f608dbe2ecb568ef118034127d77f6cccda
Brad <bradleymasciotra@gmail.com>
2025-05-02 15:44:40 -0700

    adding base client files

---
+++ Client/Client.cs
@@ -0,0 +1,95 @@
+using System.Formats.Asn1;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static Client.Constants;
+
+namespace Client;
+public class Client(string ipAddress, int port)
+{
+
+    private const int ByteArraySize = 1024;
+    private Socket socket { get; set; } = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+    public async Task Connect()
+    {
+        var endPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+        await socket.ConnectAsync(endPoint);
+    }
+
+    public async Task Send(string message, string password)
+    {
+
+        var bytes = Encoding.ASCII.GetBytes($"{message}|{password}");
+        var descriptor = await socket.SendAsync(bytes, SocketFlags.None);
+        Console.WriteLine(descriptor);
+    }
+
+    public async Task<string> Receive()
+    {
+        var buffer = new byte[ByteArraySize];
+        var numberOfBytesReceived = await socket.ReceiveAsync(buffer, SocketFlags.None);
+
+        if (numberOfBytesReceived <= 0) return string.Empty;
+        var receivedMessage = Encoding.UTF8.GetString(buffer, 0, numberOfBytesReceived);
+        Console.WriteLine($"incoming message:{receivedMessage}");
+        return receivedMessage;
+    }
+
+    // public async Task<int> SendCipher(string message, int amount)
+    // {
+    //     var mergedData = $"{message}|{amount}";
+    //     var messageBytes = Encoding.ASCII.GetBytes(mergedData);
+    //     var descriptor = await Socket.SendAsync(messageBytes, SocketFlags.None);
+    //     return descriptor;
+    // }
+
+    // public async Task<string> ReceiveData()
+    // {
+    //     var buffer = new byte[ByteArraySize];
+    //     var numberOfBytesReceived = await Socket.ReceiveAsync(buffer, SocketFlags.None);
+
+    //     if (numberOfBytesReceived <= NoBytes) return string.Empty;
+    //     var receivedMessage = Encoding.UTF8.GetString(buffer, 0, numberOfBytesReceived);
+    //     return receivedMessage;
+    // }
+
+    // public string Decrypt(string message, int shift)
+    // {
+    //     var builder = new StringBuilder();
+
+    //     foreach (var letter in message)
+    //     {
+    //         if (char.IsLetter(letter))
+    //         {
+    //             var offset = char.IsUpper(letter) ? UpperAscii : LowerAscii;
+    //             var decrypted = (char)(((letter - offset - shift) % AsciiShift + AsciiShift) % AsciiShift + offset);
+    //             builder.Append(decrypted);
+    //         }
+    //         else if (char.IsDigit(letter))
+    //         {
+    //             var digit = letter - Zero;
+    //             var decryptedDigit = (digit - shift % NumericShift + NumericShift) % NumericShift;
+    //             var decrypted = (char)(decryptedDigit + Zero);
+    //             builder.Append(decrypted);
+    //         }
+    //         else
+    //         {
+    //             builder.Append(letter);
+    //         }
+    //     }
+
+    //     return builder.ToString();
+    // }
+
+
+    public void Teardown()
+    {
+        socket.Close();
+    }
+
+    public void DisplayMessage(string message)
+    {
+        Console.WriteLine($"The Decrypted Message is: \"{message}\"");
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Client;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        try
+        {
+            var message = args[0];
+            var password = args[1];
+            var ipAddress = args[2];
+            var port = int.Parse(args[3]);
+            var client = new Client(ipAddress,port);
+
+            await client.Connect();
+            await client.Send(message,password);
+            var data = await client.Receive();
+            client.Teardown();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
